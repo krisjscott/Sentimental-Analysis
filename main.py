ba385cce88db02341526f3c36753bd5d7595d0e2
Krisjscott <krishkumar6566@gmail.com>
2025-11-05 10:59:09 +0530

    updating logistic regression code

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from nltk.corpus import twitter_samples
 from nltk.tokenize import word_tokenize
 import matplotlib.pyplot as plt
 import numpy as np
+import re                                  # library for regular expression operations
+import string                              # for string operations
 
+from nltk.corpus import stopwords          # module for stop words that come with NLTK
+from nltk.stem import PorterStemmer        # module for stemming
+from nltk.tokenize import TweetTokenizer   # module for tokenizing strings
 nltk.download('punkt')
 nltk.download('twitter_samples')
 
@@ -20,3 +25,78 @@ train_neg  = all_negative_tweets[:4000]
 train_x = train_pos + train_neg 
 
 print("Number of tweets: ", len(train_x))
+fig = plt.figure(figsize=(5, 5))
+
+# labels for the two classes
+labels = 'Positives', 'Negative'
+
+# Sizes for each slide
+sizes = [len(all_positive_tweets), len(all_negative_tweets)] 
+
+# Declare pie chart, where the slices will be ordered and plotted counter-clockwise:
+plt.pie(sizes, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+
+# Equal aspect ratio ensures that pie is drawn as a circle.
+plt.axis('equal')  
+
+# Display the chart
+plt.show()
+
+print('\033[92m' + all_positive_tweets[random.randint(0,5000)])
+
+# print negative in red
+print('\033[91m' + all_negative_tweets[random.randint(0,5000)])
+
+nltk.download('stopwords')
+
+print('\033[92m' + tweet)
+print('\033[94m')
+
+# remove old style retweet text "RT"
+tweet2 = re.sub(r'^RT[\s]+', '', tweet)
+
+# remove hyperlinks
+tweet2 = re.sub(r'https?://[^\s\n\r]+', '', tweet2)
+
+# remove hashtags
+# only removing the hash # sign from the word
+tweet2 = re.sub(r'#', '', tweet2)
+
+print(tweet2)
+print()
+print('\033[92m' + tweet2)
+print('\033[94m')
+
+# instantiate tokenizer class
+tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True,
+                               reduce_len=True)
+
+# tokenize tweets
+tweet_tokens = tokenizer.tokenize(tweet2)
+
+print()
+print('Tokenized string:')
+print(tweet_tokens)
+stopwords_english = stopwords.words('english') 
+
+print('Stop words\n')
+print(stopwords_english)
+
+print('\nPunctuation\n')
+print(string.punctuation)
+
+print()
+print('\033[92m')
+print(tweet_tokens)
+print('\033[94m')
+
+tweets_clean = []
+
+for word in tweet_tokens: # Go through every word in your tokens list
+    if (word not in stopwords_english and  # remove stopwords
+        word not in string.punctuation):  # remove punctuation
+        tweets_clean.append(word)
+
+print('removed stop words and punctuation:')
+print(tweets_clean)
