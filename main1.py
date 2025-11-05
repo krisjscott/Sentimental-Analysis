ab90b43930ceaead8c769682faa2517a77de82bd
Krisjscott <krishkumar6566@gmail.com>
2025-11-05 18:28:51 +0530

    Adding code to the main1.py file

---
+++ main1.py
@@ -0,0 +1,99 @@
+import numpy as np 
+import pandas as pd 
+
+import matplotlib.pyplot as plt 
+from utils import confidence_ellipse 
+
+data = pd.read_csv('./data/bayes_features.csv'); 
+fig, ax = plt.subplots(figsize = (8, 8)) 
+
+colors = ['red', 'green'] # Define a color palete
+sentiments = ['negative', 'positive'] 
+
+index = data.index
+
+
+for sentiment in data.sentiment.unique():
+    ix = index[data.sentiment == sentiment]
+    ax.scatter(data.iloc[ix].positive, data.iloc[ix].negative, c=colors[int(sentiment)], s=0.1, marker='*', label=sentiments[int(sentiment)])
+
+ax.legend(loc='best')    
+    
+plt.xlim(-250,0)
+plt.ylim(-250,0)
+
+plt.xlabel("Positive") # x-axis label
+plt.ylabel("Negative") # y-axis label
+plt.show()
+
+fig, ax = plt.subplots(figsize = (8, 8))
+
+colors = ['red', 'green'] # Define a color palete
+sentiments = ['negative', 'positive'] 
+index = data.index
+
+# Color base on sentiment
+for sentiment in data.sentiment.unique():
+    ix = index[data.sentiment == sentiment]
+    ax.scatter(data.iloc[ix].positive, data.iloc[ix].negative, c=colors[int(sentiment)], s=0.1, marker='*', label=sentiments[int(sentiment)])
+
+# Custom limits for this chart
+plt.xlim(-200,40)  
+plt.ylim(-200,40)
+
+plt.xlabel("Positive") # x-axis label
+plt.ylabel("Negative") # y-axis label
+
+data_pos = data[data.sentiment == 1] # Filter only the positive samples
+data_neg = data[data.sentiment == 0] # Filter only the negative samples
+
+# Print confidence ellipses of 2 std
+confidence_ellipse(data_pos.positive, data_pos.negative, ax, n_std=2, edgecolor='black', label=r'$2\sigma$' )
+confidence_ellipse(data_neg.positive, data_neg.negative, ax, n_std=2, edgecolor='orange')
+
+# Print confidence ellipses of 3 std
+confidence_ellipse(data_pos.positive, data_pos.negative, ax, n_std=3, edgecolor='black', linestyle=':', label=r'$3\sigma$')
+confidence_ellipse(data_neg.positive, data_neg.negative, ax, n_std=3, edgecolor='orange', linestyle=':')
+ax.legend(loc='lower right')
+
+plt.show()
+
+data2 = data.copy() # Copy the whole data frame
+
+# The following 2 lines only modify the entries in the data frame where sentiment == 1
+data2.negative[data.sentiment == 1] =  data2.negative * 1.5 + 50 # Modify the negative attribute
+data2.positive[data.sentiment == 1] =  data2.positive / 1.5 - 50 # Modify the positive attribute 
+
+# Plot the samples using columns 1 and 2 of the matrix
+fig, ax = plt.subplots(figsize = (8, 8))
+
+colors = ['red', 'green'] # Define a color palete
+sentiments = ['negative', 'positive'] 
+index = data2.index
+
+# Color base on sentiment
+for sentiment in data2.sentiment.unique():
+    ix = index[data2.sentiment == sentiment]
+    ax.scatter(data2.iloc[ix].positive, data2.iloc[ix].negative, c=colors[int(sentiment)], s=0.1, marker='*', label=sentiments[int(sentiment)])
+
+#ax.scatter(data2.positive, data2.negative, c=[colors[int(k)] for k in data2.sentiment], s = 0.1, marker='*')  # Plot a dot for tweet
+# Custom limits for this chart
+plt.xlim(-200,40)  
+plt.ylim(-200,40)
+
+plt.xlabel("Positive") # x-axis label
+plt.ylabel("Negative") # y-axis label
+
+data_pos = data2[data2.sentiment == 1] # Filter only the positive samples
+data_neg = data[data2.sentiment == 0] # Filter only the negative samples
+
+# Print confidence ellipses of 2 std
+confidence_ellipse(data_pos.positive, data_pos.negative, ax, n_std=2, edgecolor='black', label=r'$2\sigma$' )
+confidence_ellipse(data_neg.positive, data_neg.negative, ax, n_std=2, edgecolor='orange')
+
+# Print confidence ellipses of 3 std
+confidence_ellipse(data_pos.positive, data_pos.negative, ax, n_std=3, edgecolor='black', linestyle=':', label=r'$3\sigma$')
+confidence_ellipse(data_neg.positive, data_neg.negative, ax, n_std=3, edgecolor='orange', linestyle=':')
+ax.legend(loc='lower right')
+
+plt.show()
